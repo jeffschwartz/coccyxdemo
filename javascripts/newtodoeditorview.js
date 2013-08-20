aab724fc8b9b9f553347a465e27704c0ad7ec631
jeffschwartz <jefftschwartz@gmail.com>
2013-08-19 20:38:23 -0400

    A wip.

--- javascripts/newtodoeditorview.js
+++ javascripts/newtodoeditorview.js
@@ -4,6 +4,9 @@ define([], function(){
     return {
         domTarget: 'div.new-todo-container',
         template: '#new-todo-item-template',
+        domEvents: {
+            'click div.new-todo-container'
+        }
         render: function(){
             this.$domTarget.html(this.$(this.template).html());
         }
